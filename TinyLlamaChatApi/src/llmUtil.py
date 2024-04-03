9b344d458f9f820b6a30e2e45fd58305ac972ba5
大溝桂 <kay.akubicharm.dev@gmail.com>
2024-04-04 04:19:06 +0900

    update

--- TinyLlamaChatApi/src/llmUtil.py
+++ TinyLlamaChatApi/src/llmUtil.py
@@ -1,23 +1,40 @@
+import ast
 from llama_cpp import Llama
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import sys
+import datetime
 
 llm = Llama(model_path="tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf", verbose=False)
+# llm = Llama(model_path="ELYZA-japanese-Llama-2-7b-fast-q2_K.gguf", verbose=False)
+
+agentname = "akubi"
+current_date = datetime.datetime.now()
+formatted_date = current_date.strftime("%m/%d/%Y")
+baseprompt = "You are %s, a highly intelligent assistant. Keep your answers brief and accurate. Current date is %s." % (agentname, formatted_date)
+context = [{"role": "system", "content": baseprompt}] 
+
 
 class LlmUtil(Resource):
 
     def post(self):
-        text = request.get_json().get("text") or ""
-        system = request.get_json().get("system") or ""
-        print(request.get_json())
-        output = llm("<user>\n" + text + "\n<assistant>\n" +  "\n<system>\n" + system, max_tokens=40)
+        role = request.get_json().get("role") or ""
+        prompt = request.get_json().get("text") or ""
+        print(role + " " + prompt)
+        context.append({"role": role, "content": "Okay, let's get started."})
+        context.append({"role": "user", "content": prompt})
+
+        output = llm(
+            prompt,
+            max_tokens=128,
+            temperature=0.3
+        )
+
         print(output)
-        return (output['choices'][0]["text"] + "...")
- 
+        return (output['choices'][0])        
 
 
 if __name__ == '__main__':
     util = LlmUtil();
-    res = util.get(sys.argv[1]);
+    res = util.post(sys.argv[1]);
     print (res)
